e9228c7519a639641b444374b264cf7ce9c924cc
Mauricio Fernández <elmaildelmauro@gmail.com>
2024-07-18 19:29:49 -0400

    Initial commit with clean history

---
+++ src/lib/__init__.py
@@ -0,0 +1,4 @@
+import get_audio
+import spotify_credentials
+import spotify_api
+import gui
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import time
+import lib
+
+def main():
+    pass
+
+if __name__ == "__name__":
+    main()
+
