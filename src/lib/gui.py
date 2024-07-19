3a50bc0c9de0d19eb965b021ce9a3a626a97e9a1
Mauricio Fernández <elmaildelmauro@gmail.com>
2024-07-19 18:46:25 -0400

    customtkinter test

--- src/lib/gui.py
+++ src/lib/gui.py
@@ -1,1 +1,30 @@
-import PySide6
+import customtkinter as ctk
+
+def off():
+    print("off")
+
+
+#def state_button(state):
+   # return "On" if state else "Off"
+        
+
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("dark-blue")
+
+#Window
+height_window = 720
+width_window = 700
+app = ctk.CTk()
+
+app.title("Spydio")
+app.geometry(f"{height_window}x{width_window}")
+
+my_entry = ctk.CTkEntry(app, placeholder_text="Enter your Client ID", text_color="#ffffff")
+my_entry.pack(pady=(340, 10))
+
+power_button = ctk.CTkButton(app, width=100, height=45, text="Play", command=off, fg_color="#b298dc", hover_color="#a663cc", corner_radius=20, state= "normal")
+power_button.pack(pady=(0, 10))
+
+
+#Mainloop
+app.mainloop()
