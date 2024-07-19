301fcb17384e178fde207f5ea1e4f6fa16ca6181
Felipe Gajardo <314pew@gmail.com>
2024-07-19 18:43:11 -0400

    Cambie los boleanos de que retorna la funcion

--- src/lib/get_audio.py
+++ src/lib/get_audio.py
@@ -1,6 +1,6 @@
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 
-def is_any_browser_playing_audio(browser_names=(
+def is_mute(browser_names=(
     "chrome.exe", "firefox.exe", "msedge.exe", "iexplore.exe", "opera.exe",
     "vivaldi.exe", "brave.exe", "chromium.exe", "safari.exe", "maxthon.exe",
     "yandex.exe", "palemoon.exe", "waterfox.exe", "seamonkey.exe", "avant.exe",
@@ -11,20 +11,11 @@ def is_any_browser_playing_audio(browser_names=(
         if session.Process:
             process_name = session.Process.name().lower()
             if process_name in (name.lower() for name in browser_names):
-                try:
-                    volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-                    if volume.GetMasterVolume() > 0:
-                        return True
-                except Exception as e:
-                    print(f"Error al consultar el volumen para {process_name}: {e}")
+                volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+                if volume.GetMasterVolume() > 0:
+                    return False
+    return True 
 
-    return False
 
-# Ejemplo de uso
-if __name__ == "__main__":
-    if is_any_browser_playing_audio():
-        print("Alguno de los navegadores está reproduciendo audio.")
-    else:
-        print("Ninguno de los navegadores está reproduciendo audio.")
 
 
