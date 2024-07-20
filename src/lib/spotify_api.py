5ddb98b0705221317d8711fc5ad42a24c15884c3
Mauricio Fernández <elmaildelmauro@gmail.com>
2024-07-19 21:08:33 -0400

    Module to get playlist and module documentation

--- src/lib/spotify_api.py
+++ src/lib/spotify_api.py
@@ -6,10 +6,10 @@ class User:
     def __init__(self, client_id, client_secret, redirect_uri, scope):
         """
         Args:
-            client_id: Client ID from Spotify for Developers
-            client_secret: Client Secret from Spotify for Developers
-            redirect_uri: Redirect URI from Spotify for Developers
-            scope: permissions allowed to the program to work with spotify
+            client_id(str): Client ID from Spotify for Developers
+            client_secret(str): Client Secret from Spotify for Developers
+            redirect_uri(str): Redirect URI from Spotify for Developers
+            scope(str): permissions allowed to the program to work with spotify
         """
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
                                 client_id= client_id,
@@ -18,13 +18,20 @@ class User:
                                 scope= scope
                                 ))
     
-    @staticmethod
+    def get_user_information(self):
+        """
+        Returns the user's name and profile picture
+        """
+        user_info = self.sp.current_user()
+        return user_info["display_name"], user_info["images"]
+    
+
     def get_current_playback(self):
         """
         Extracts the information of the currently playing song
 
-        Args:
-            
+        Return:
+            dict: {"song": song, "artist": artist, "album": album, "album_picture": album_picture}
         """
         playback_info = self.sp.current_playback()
         return {
@@ -34,19 +41,61 @@ class User:
             "album_picture": playback_info["item"]["album"]["images"][0]["url"]
         }
 
-    def start_playback(self):
-        self.sp.start_playback()
+    def start_playback(self, playlist_id=None):
+        """
+        Plays a specific playlist if specified; otherwise, only Spotify is played.
+
+        Args:
+            playlist_id(str): Playlist ID
+        """
+
+        if playlist_id:
+            self.sp.start_playback(context_uri=f"spotify:playlist:{playlist_id}")
+        else:
+            self.sp.start_playback()
         self.unmute()
     
     def pause_playback(self):
         self.sp.pause_playback()
         self.mute()
     
-    def unmute(self):
-        for i in range(25, 75, 6):
+    def unmute(self, step):
+        """
+        Progressively increases the volume until it reaches the desired level.
+
+        Args:
+            step (int): The step value to increase the volume. For example, 3 will increase the volume by 3 units at each step.
+        """
+        for i in range(25, 75, step):
             self.sp.volume(i)
 
-    def mute(self):
-        for i in range(75, 0, -6):
+    def mute(self, step):
+        """
+        Progressively decreases the volume until muted.
+
+        Args:
+            step (int): The negative step value to decrease the volume. For example, -3 will decrease the volume by 3 units at each step.
+        """
+        for i in range(75, 0, step):
             self.sp.volume(i)    
     
+    def get_playlist(self):
+        """
+        Retrieves the current user's playlists and their basic information.
+
+        Returns:
+            list of dict: A list of dictionaries, each containing:
+                - name (str): The name of the playlist.
+                - id (str): The Spotify ID of the playlist.
+                - tracks (int): The total number of tracks in the playlist.
+        """
+        playlists = self.sp.current_user_playlists()
+        playlist_info = list()
+
+        for playlist in playlists["items"]:
+            playlist_info.append({
+                "name": playlist["name"],
+                "id": playlist["id"],
+                "tracks": playlist["tracks"]["total"]
+            })
+        return playlist_info
