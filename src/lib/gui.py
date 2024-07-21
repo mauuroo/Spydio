676171b79dca4b2c9c059e34098f524973722aa5
Felipe Gajardo <314pew@gmail.com>
2024-07-21 17:04:22 -0400

    Ningun cambio solo por si acaso para hacer un pull

--- src/lib/gui.py
+++ src/lib/gui.py
@@ -8,8 +8,8 @@ def off():
    # return "On" if state else "Off"
         
 
-ctk.set_appearance_mode("system")
-ctk.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("Dark")
+ctk.set_default_color_theme("green")
 
 #Window
 height_window = 720
@@ -19,7 +19,7 @@ app = ctk.CTk()
 app.title("Spydio")
 app.geometry(f"{height_window}x{width_window}")
 
-my_entry = ctk.CTkEntry(app, placeholder_text="Enter your Client ID", text_color="#ffffff")
+my_entry = ctk.CTkEntry(app, placeholder_text="Enter your Client ID", text_color="6a6e73")
 my_entry.pack(pady=(340, 10))
 
 power_button = ctk.CTkButton(app, width=100, height=45, text="Play", command=off, fg_color="#b298dc", hover_color="#a663cc", corner_radius=20, state= "normal")
