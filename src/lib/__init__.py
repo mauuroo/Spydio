80ec6ecdf9193763c9c8c72c1e8dc286701a4013
Felipe Gajardo <314pew@gmail.com>
2024-07-21 18:51:24 -0400

    "No se me pide hacer commit"

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,5 +1,5 @@
 from .get_audio import is_mute
 #from .gui import *
-from .spotify_config import *
+from .spotify_credentials import *
 from .spotify_api import User
 from .last_key_pressed import get_last_key
