a592df99f3ebc4ab33f38aec7b8d8445b93e6873
Mauricio Fernández <elmaildelmauro@gmail.com>
2024-08-02 00:07:09 -0400

    improvements and error handling

--- src/main.py
+++ src/main.py
@@ -1,16 +1,38 @@
 import time
 import lib as lb
+from psutil import process_iter
+import subprocess
+
+def is_spotify_running():
+    """
+    Checks if Spotify is currently running.
+
+    Returns:
+        bool: True if Spotify is running, False otherwise.
+    """
+
+    for proc in process_iter(['name']):
+        if proc.info["name"] == "Spotify.exe":  # For Windows
+            return True
+        elif proc.info["name"] == "Spotify":  # For macOS and Linux
+            return True
+    return False
+
+
 
 def main():
     user = lb.User(client_id=lb.CLIENT_ID, client_secret=lb.CLIENT_SECRET, redirect_uri=lb.REDIRECT_URI, scope=lb.SCOPE)
     state = True
 
+    #Makes sure to have spotify open for the correct execution of the program
+    if not is_spotify_running():
+        subprocess.Popen("spotify")
+        time.sleep(10)
 
     try:
         click_detector = lb.on_click_allowed.ClickDetector()
-        click_detector.select_monitor(0)
+        click_detector.select_monitor(1)
 
-            
         while True: 
             if lb.get_audio.is_mute():
                 if state:
@@ -28,7 +50,7 @@ def main():
                     user.pause_playback()
                     state = True
 
-            time.sleep(0.2)
+            time.sleep(0.25)
 
 
     finally:
