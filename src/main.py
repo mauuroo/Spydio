46ec5586aa2a7304ec2ea4bba32f1c947b699f6c
Mauricio Fernández <elmaildelmauro@gmail.com>
2024-09-14 20:45:35 -0300

    Solution to specific error case

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def is_spotify_running():
             return True
         elif proc.info["name"] == "Spotify":  # For macOS and Linux
             return True
-    return False
+    return False    
 
 
 
@@ -37,14 +37,13 @@ def main():
             if lb.get_audio.is_mute():
                 if state:
                     last_key = lb.last_key_pressed.get_last_key()
-                    if last_key not in ["left", "right"]:
-                        if  click_detector.has_left_clicked():
-                            if click_detector.approximate_pause_click_in_monitor():
-                                user.start_playback()  
-                                state = False
-                        else:
+                    if  click_detector.has_left_clicked():
+                        if click_detector.approximate_pause_click_in_monitor():
                             user.start_playback()  
                             state = False
+                    elif last_key not in ["left", "right"]:
+                        user.start_playback()
+                        state = False
             else:
                 if not state:
                     user.pause_playback()
